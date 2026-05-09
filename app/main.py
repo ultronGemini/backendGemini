6b2a7d67155c8fb5bc5b4f905d31389a24b8ce30
herrdelta83 <bailondelta@gmail.com>
2026-05-09 14:19:24 -0600

    Added CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.creative_routes import router
 
 app = FastAPI(title="Creative Block API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
